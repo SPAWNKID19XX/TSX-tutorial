d46c7c3e3fbe6c56f947ce9492ba3f50261481b2
boris-isac <borisisac@gmail.com>
2026-02-04 16:47:19 +0000

    [UPDATE] persons method inherance

--- oopInheritance.ts
+++ oopInheritance.ts
@@ -2,10 +2,18 @@ class Person {
     firstName: string = "";
     lastName: string = "";
     age: number = 0;
+
+    sayHello() {
+        return `My name is ${this.firstName} ${this.lastName} and I am ${this.age} Years old`
+    }
 }
 
 class Emplloyeer extends Person {
     departament: string = ""
+
+    sayHello(): string {
+        return super.sayHello() + `and I am ${this.departament} specialist`
+    }
 }
 
 let empl= new Emplloyeer()
@@ -15,4 +23,4 @@ empl.lastName="Isac"
 empl.age=38
 empl.departament="IT"
 
-console.log(empl);
+console.log(empl.sayHello());
