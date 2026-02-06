6e857b6d8bf2bf407dfe5e9ace6a8fe6768ef06d
boris-isac <borisisac@gmail.com>
2026-02-06 20:24:24 +0000

    [UPDATE] inherit methods with super

--- oopInheritance.ts
+++ oopInheritance.ts
@@ -14,6 +14,12 @@ class Person {
     sayHello() {
         return `My name is ${this.firstName} ${this.lastName} and I am ${this.age} Years old`
     }
+
+    sellStock(simbols: string, numberOfShares: number) {
+        console.log(`Selling... ${numberOfShares} items of ${simbols}`);
+    }
+
+
 }
 
 class Emplloyeer extends Person {
@@ -31,6 +37,17 @@ class Emplloyeer extends Person {
     sayHello(): string {
         return super.sayHello() + `and I am ${this.departament} specialist`
     }
+
+
+    sellStock(simbols: string, numberOfShares: number) {
+        super.sellStock(simbols, numberOfShares);
+        this.reportInfo(simbols, numberOfShares);
+    }
+
+    private reportInfo(simbols: string, numberOfShares: number) {
+        console.log(`${this.firstName} ${this.lastName} from ${this.departament} departament, sold ${numberOfShares} of ${simbols}`); 
+    }
+    
 }
 
 let empl = new Emplloyeer('Aaron', 'isac',12,'pupsik2','IT')
@@ -43,4 +60,7 @@ let per = new Person("Boris","Isac", 38,'Pupsik1')
 console.log(empl.sayHello());
 
 console.log(per.sayHello());
-console.log(per)
+
+empl.sellStock("microsoft", 5);
+
+
